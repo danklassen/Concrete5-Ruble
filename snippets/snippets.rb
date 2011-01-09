321fa021e24831551b6d205e98cc690f811d4f57
Dan Klassen <dan@tripleiwebsolutions.com>
2011-01-09 15:49:37 -0800

    snippet update

--- snippets/snippets.rb
+++ snippets/snippets.rb
@@ -7,15 +7,15 @@ with_defaults :scope => 'source.php' do
   
   snippet "Text Field" do |s|
     s.trigger = 'ctf'
-    s.expansion = '<?php print $form->label(${1:\'field_name\'}, ${2:\'Field Label\'});
-      <?php print $form->text(${1});
+    s.expansion = '<?php print \$form->label(${1:\'field_name\'}, ${2:\'Field Label\'});
+<?php print \$form->text(${1});
     '
   end
   
   snippet "Text Area Field" do |s|
     s.trigger = 'ctaf'
-    s.expansion = '<?php print $form->label(${1:\'field_name\'}, ${2:\'Field Label\'});
-      <?php print $form->textarea(${1});
+    s.expansion = '<?php print \$form->label(${1:\'field_name\'}, ${2:\'Field Label\'});
+<?php print \$form->textarea(${1});
     '
   end
 end
